75339e691f8d91fba54105e7b7ae4c0bdc2f0d32
Regine Margareta <gin4work@outlook.com>
2019-01-12 23:27:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var greeting = "Hello Everybody!";
 
-var specialGuest = "Neil deGrasse Tyson" ; "Hello," + specialGuest + "!" ;
-
-var greetSpecialGuest = "Neil deGrasse Tyson"; "Hello," + greetSpecialGuest + "!";
-
-var topic = "space";
-
-var conversation = "topic" ; `let's talk about ${topic}`;
